56c4754ba7e1ba4cb3f86a0f7763a339b6a95fdb
Meghana8204 <meghanaar222@gmail.com>
2025-07-21 14:57:31 +0530

    hello

--- demo.js
+++ demo.js
@@ -1,3 +1,4 @@
 console.log('git demo');
 console.log('This is a demo for git commands in VSCode');
 console.log('This is a simple demonstration of git functionality in VSCode');
+console.log('hello');
